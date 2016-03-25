3d1ed0fcbae54e36ba86767f37c96c4d32df3392
skafis <franciskipchumba5@gmail.com>
2016-03-25 21:29:25 +0300

    generated files

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,9 @@ class Post(models.Model):
 	author =  models.ForeignKey('auth.User')
 	title = models.CharField(max_length=200)
 	text = models.TextField()
-	created_date = models.DateTimeField(default=timezone.now)
+	created_date = models.DateTimeField(
+
+default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
 
 def Publish(self):
