def9b413d9718a17153e1b462ef6d40f9be6b2d8
skafis <franciskipchumba5@gmail.com>
2016-04-17 17:23:53 +0300

    add 404 querry

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render_to_response, redirect
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from django.core.urlresolvers import reverse
+
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render (request, 'blog/post_list.html',{'posts':posts})
 
 def post_detail (request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html',{'post':post})
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
     if request.method == "POST":
