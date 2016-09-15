248c3e21f57dee71ee3edc870c888f69f5599a0e
franck <fkipchumba@intellisoftplus.com>
2016-09-15 16:39:52 +0300

    fix pagination on bloglist

--- blog/views.py
+++ blog/views.py
@@ -11,21 +11,22 @@ from django.core.urlresolvers import reverse
 # Create your views here.
 def post_list(request):
     posts_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    paginator = Paginator(posts_list, 3)  # Show 25 contacts per page
+    paginator = Paginator(posts_list, 4)  # Show 25 contacts per page
     page_request_var = "page"
     page = request.GET.get(page_request_var)
     try:
-        posts = paginator.page(page)
+        queryset = paginator.page(page)
     except PageNotAnInteger:
         # If page is not an integer, deliver first page.
-        posts = paginator.page(1)
+        queryset = paginator.page(1)
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
-        posts = paginator.page(paginator.num_pages)
+        queryset = paginator.page(paginator.num_pages)
 
     context = {
-        'posts':posts_list,
-        'paginator':paginator,
+        # 'posts':posts_list,
+        'posts':queryset, 
+        "page_request_var": page_request_var,
     }
 
     return render (request, 'blog/post_list.html',context)
