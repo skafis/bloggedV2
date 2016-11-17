49e7cd79fc344a1b8e7e21cb72508a022443baef
frank <franciskipchumba5@gmail.com>
2016-11-18 02:43:19 +0300

    add pagedown

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,12 @@
 from django import forms
+
+from pagedown.widgets import PagedownWidget
+
 from .models import Post
 
 class PostForm (forms.ModelForm):
-    
-    class Meta:
-        model = Post
-        fields = ('title', 'text',)
+	text = forms.CharField(widget=PagedownWidget)
+	class Meta:
+		model = Post
+		fields = ('title', 'text',)
 
