c1db31c378280ba7144e191b37a7203c5e45044a
frank <franciskipchumba5@gmail.com>
2016-11-18 01:22:05 +0300

    add markdown

--- blog/models.py
+++ blog/models.py
@@ -10,9 +10,7 @@ class Post(models.Model):
 	title = models.CharField(max_length=200)
 	slug = models.SlugField(unique=True)
 	text = models.TextField()
-	created_date = models.DateTimeField(
-
-default=timezone.now)
+	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
 
 	def Publish(self):
